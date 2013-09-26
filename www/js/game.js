0798f0a938a4de8c26a6c044f66cc1b28e892452
unknown <enascimentosousa@gmail.com>
2013-09-26 16:42:18 -0300

    Testando as primeiras funcionalidades

---
+++ www/js/game.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	
+	var character = null;
+	
+	$('li.select-char').click(function(){
+		character = $(this).attr('id');
+		
+		$('li.selected-char').each(function(){
+			$(this).removeClass('selected-char');
+		});
+		
+		$(this).addClass('selected-char');
+	});
+	
+	$('input#start-game').click(function(){
+		if(character == null){
+			alert('por favor escolha');
+		}else{
+			$('div#choose-animal').hide();
+			$('div#play-game').show();
+		}
+	});
+	
+	$('td.cel-game').click(function(){ alert('clicou');
+		var thisValue = $(this).html();
+		
+		if(thisValue == 'dog' || thisValue == 'cat'){
+			alert('escolha outra');
+		}else{
+			$(this).html(character);
+		}
+	});
+	
+});
